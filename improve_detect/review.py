157b7af80e38c3752c5fdb65006b8f2ec29326f3
taxfree_python <taxfreepython@gmail.com>
2021-12-04 13:26:16 +0900

    improve

--- improve_detect/review.py
+++ improve_detect/review.py
@@ -6,49 +6,45 @@ from multiprocessing import Pool
 import pyocr
 import re
 import time
+import csv
 
-
-class Optimizer:
-
-    def review(self, path):
-        '''
-        for i in range(1):
-            original = cv2.imread(f'datasets/dataset_1/image_{i}.png')
-            img = self.comb(original)
-            original = Image.fromarray(original)
-
-            imgs = [original, img]
-            ans = p.map(self.read_num_img, imgs)
-
-            #cv2.imwrite(f'datum/diff/diff_{i}.png', np.array(img))
-
-            print(ans)
-            if ans[1][0] == corrects[i - 1] and ans[1][1] == corrects[i - 1]:
-                correct += 1
-            elif ans[1][0] == corrects[i - 1] or ans[1][1] == corrects[i - 1]:
-                print(f'{i}, {corrects[i - 1]}, {ans[0]}→{ans[1]}')
-                probably += 1
-            elif corrects[i - 1] != 0:
-                print(f'{i}, {corrects[i - 1]}, {ans[0]}→{ans[1]}')
-                wrong += 1
-
-            if i % 10 == 0:
-                print('working')
-        return f'reslt = {correct}, {probably}, {wrong}'
-        '''
-
-        #model_1
+class Reviewer:
+    def review(self, path, method):
         start = time.time()
-        with open(f'{path}/correct.txt') as f:
+
+        with open(f'{sample_name}/correct.txt') as f:
             corrects = [int(s) for s in f.readlines()]
 
         length = len(corrects)
         score = [0, 0, 0, 0] #both, ans[0], ans[1], neither
 
         index = [i for i in range(length)]
-        p = Pool(6)
-        ans_eng = p.map(self.read_num_img_eng, index)
-        ans_snum = p.map(self.read_num_img_snum, index)
+
+        if method == 1:
+            p = Pool(6)
+            ans_eng = p.map(self.read_num_img_eng_1, index)
+            ans_snum = p.map(self.read_num_img_snum_1, index)
+        elif method == 2:
+            p = Pool(6)
+            ans_eng = p.map(self.read_num_img_eng_2, index)
+            ans_snum = p.map(self.read_num_img_snum_2, index)
+        elif method == 3:
+            p = Pool(6)
+            ans_eng = p.map(self.read_num_img_eng_3, index)
+            ans_snum = p.map(self.read_num_img_snum_3, index)
+        elif method == 4:
+            p = Pool(6)
+            ans_eng = p.map(self.read_num_img_eng_4, index)
+            ans_snum = p.map(self.read_num_img_snum_4, index)
+        elif method == 5:
+            p = Pool(6)
+            ans_eng = p.map(self.read_num_img_eng_5, index)
+            ans_snum = p.map(self.read_num_img_snum_5, index)
+        elif method == 6:
+            p = Pool(6)
+            ans_eng = p.map(self.read_num_img_eng_6, index)
+            ans_snum = p.map(self.read_num_img_snum_6, index)
+
 
         cnt = 0
         for eng, snum, correct in zip(ans_eng, ans_snum, corrects):
@@ -59,112 +55,175 @@ class Optimizer:
             elif snum == correct:
                 score[2] += 1
             else:
-                print(cnt, eng, snum, correct)
                 score[3] += 1
             cnt += 1
 
         fin = time.time()
-        print(f'used time is {fin - start}')
+        t = round(fin - start, 2)
+        print(t)
+
+        with open('method_1.csv', 'a') as f:
+            write = csv.writer(f)
+
+
+
+            write.writerow([param, number] + score)
+
+
+
+
         return score
 
 
-        '''
-        #model_2
-        start = time.time()
-        with open(f'{path}/correct.txt') as f:
-            corrects = [int(s) for s in f.readlines()]
-
-        length = len(corrects)
-        score = [0, 0, 0, 0] #both, ans[0], ans[1], neither
-
-        p = Pool(6)
-        for i in range(length):
-            img = self.comb(cv2.imread(f'{path}/image_{i}.png'))
-            datum = [(img, 'eng'), (img, 'snum')]
-
-            ans = p.map(self.read_num_img, datum)
-
-            if ans[0] == corrects[i] == ans[0] == ans[1]:
-                score[0] += 1
-            elif ans[0] == corrects[i]:
-                score[1] += 1
-            elif ans[1] == corrects[i]:
-                score[2] += 1
-            else:
-                score[3] += 1
-
-        fin = time.time()
-        print(f'used time is {fin - start}')
-        return score
-        '''
-
-
-    def read_num_img_eng(self, index):
+    def read_num_img_eng_1(self, index):
         tool = pyocr.get_available_tools()[0]
 
-        img = self.comb(cv2.imread(f'{path}/image_{index}.png'))
+        img = self.method_1(cv2.imread(f'{sample_name}/image_{index}.png'))
         num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'eng', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
 
         return num
 
 
-    def read_num_img_snum(self, index):
+    def read_num_img_eng_2(self, index):
         tool = pyocr.get_available_tools()[0]
 
-        img = self.comb(cv2.imread(f'{path}/image_{index}.png'))
+        img = self.method_2(cv2.imread(f'{sample_name}/image_{index}.png'))
+        num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'eng', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
+
+        return num
+
+
+    def read_num_img_eng_3(self, index):
+        tool = pyocr.get_available_tools()[0]
+
+        img = self.method_3(cv2.imread(f'{sample_name}/image_{index}.png'))
+        num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'eng', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
+
+        return num
+
+
+    def read_num_img_eng_4(self, index):
+        tool = pyocr.get_available_tools()[0]
+
+        img = self.method_4(cv2.imread(f'{sample_name}/image_{index}.png'))
+        num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'eng', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
+
+        return num
+
+
+    def read_num_img_eng_5(self, index):
+        tool = pyocr.get_available_tools()[0]
+
+        img = self.method_5(cv2.imread(f'{sample_name}/image_{index}.png'))
+        num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'eng', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
+
+        return num
+
+
+    def read_num_img_eng_6(self, index):
+        tool = pyocr.get_available_tools()[0]
+
+        img = self.method_6(cv2.imread(f'{sample_name}/image_{index}.png'))
+        num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'eng', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
+
+        return num
+
+
+    def read_num_img_snum_1(self, index):
+        tool = pyocr.get_available_tools()[0]
+
+        img = self.method_1(cv2.imread(f'{sample_name}/image_{index}.png'))
         num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'snum', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
 
         return num
 
 
-    def read_num_img(self, datum):
+    def read_num_img_snum_2(self, index):
         tool = pyocr.get_available_tools()[0]
 
-        num_eng = re.sub(r'\D', '', tool.image_to_string(datum[0], lang = datum[1], builder = pyocr.builders.DigitBuilder(tesseract_layout = 8)))
+        img = self.method_2(cv2.imread(f'{sample_name}/image_{index}.png'))
+        num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'snum', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
 
-        ans = int(num_eng or 0)
-        return ans
-
-
-    def inv(self, diff): #二値化，白黒反転
-        ret, diff = cv2.threshold(diff, 200, 255, cv2.THRESH_BINARY_INV)
-
-        return diff
+        return num
 
 
-    def findcon(self, diff): #輪郭抽出
-        x_size, y_size = diff.shape[0], diff.shape[1]
-        edge = cv2.Canny(diff, 255, 260, True)
+    def read_num_img_snum_3(self, index):
+        tool = pyocr.get_available_tools()[0]
 
-        cv2.imwrite('edge.png', edge)
-        contours, hierarchy = cv2.findContours(edge, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        img = self.method_3(cv2.imread(f'{sample_name}/image_{index}.png'))
+        num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'snum', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
 
-        app = []
-        for i in range(len(contours)):
-            length = cv2.arcLength(contours[i], True)
-            app.append((contours[i], length))
-
-        app.sort(key = lambda x: x[1], reverse = True)
-
-        emp = np.zeros((x_size, y_size))
-        diff = cv2.drawContours(emp, contours, -1, (255, 0, 0), 2) #2
-
-        diff = Image.fromarray(diff)
-        return diff
+        return num
 
 
-    def rot(self, diff): #回転
+    def read_num_img_snum_4(self, index):
+        tool = pyocr.get_available_tools()[0]
+
+        img = self.method_4(cv2.imread(f'{sample_name}/image_{index}.png'))
+        num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'snum', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
+
+        return num
+
+
+    def read_num_img_snum_5(self, index):
+        tool = pyocr.get_available_tools()[0]
+
+        img = self.method_5(cv2.imread(f'{sample_name}/image_{index}.png'))
+        num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'snum', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
+
+        return num
+
+
+    def read_num_img_snum_6(self, index):
+        tool = pyocr.get_available_tools()[0]
+
+        img = self.method_6(cv2.imread(f'{sample_name}/image_{index}.png'))
+        num = int(re.sub(r'\D', '', tool.image_to_string(img, lang = 'snum', builder = pyocr.builders.DigitBuilder(tesseract_layout = 8))) or 0)
+
+        return num
+
+
+    def method_1(self, diff): #回転
         x_size = diff.shape[0]
         y_size = diff.shape[1]
 
-        trans = cv2.getRotationMatrix2D((int(x_size / 2), int(y_size / 2)), -2, 0.9)
-        diff = cv2.warpAffine(diff, trans, (y_size, x_size), borderValue=(255, 255, 255))
+        trans = cv2.getRotationMatrix2D((int(x_size / 2), int(y_size / 2)), param, 0.9)
+        diff = cv2.warpAffine(diff, trans, (y_size, x_size), borderValue = (255, 255, 255))
+
+        #cv2.imwrite('rot.png', diff)
+        diff = Image.fromarray(diff)
+        return diff
+
+
+    def method_2(self, diff): #二値化，白黒反転
+        ret, diff = cv2.threshold(diff, 200, 255, cv2.THRESH_BINARY_INV)
 
         diff = Image.fromarray(diff)
         return diff
 
 
-    def trimming(self, diff): #トリミング
+    def method_3(self, diff):
+        diff = cv2.GaussianBlur(diff, (7, 7,), 3)
+
+        diff = Image.fromarray(diff)
+        return diff
+
+
+    def method_4(self, diff): #輪郭抽出
+        x_size, y_size = diff.shape[0], diff.shape[1]
+        edge = cv2.Canny(diff, 255, 260, True)
+
+        #cv2.imwrite('edge.png', edge)
+        contours, hierarchy = cv2.findContours(edge, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        emp = np.zeros((x_size, y_size))
+        diff = cv2.drawContours(emp, contours, -1, (255, 0, 0), 2)
+
+        diff = Image.fromarray(diff)
+        return diff
+
+
+    def method_5(self, diff): #トリミング
         diff[168:, :45] = [[255, 255, 255]]
         diff[0:, :20] = [[255, 255, 255]]
 
@@ -172,17 +231,24 @@ class Optimizer:
         return diff
 
 
-    def comb(self, diff):
-        diff = np.array(self.inv(diff))
-        diff = np.array(self.trimming(diff))
-        diff = self.rot(diff)
+    def method_6(self, diff):
+        #diff = np.array(self.method_3(diff))
+        diff = np.array(self.method_2(diff))
+        diff = np.array(self.method_5(diff))
+        diff = np.array(self.method_1(diff))
+        diff = self.method_4(diff)
         return diff
         #return Image.fromarray(diff)
 
 
 
 
-path = 'datasets/dataset_2'
+number = 5
+sample_name = 'datasets/dataset_' + str(number)
+method = 1
+param = 4
+
+
 if __name__ == '__main__':
-    opt = Optimizer()
-    print(opt.review(path))
+    rev = Reviewer()
+    print(rev.review(sample_name, method))
